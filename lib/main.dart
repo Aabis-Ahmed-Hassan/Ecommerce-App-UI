b5a8495002ec538e4352e61602b31f0d6dc9baf3
Aabis-Ahmed-Hassan <aabisahmedhassan3@gmail.com>
2024-01-28 10:19:58 +0500

    Done with basic structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,61 @@
+import 'package:ecommere_store_ui/pages/cart.dart';
+import 'package:ecommere_store_ui/pages/favourite_page.dart';
+import 'package:ecommere_store_ui/pages/home_page.dart';
+import 'package:ecommere_store_ui/pages/profile_page.dart';
+import 'package:ecommere_store_ui/pages/shop_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var currentItem = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Text('Hello World'),
+      home: Scaffold(
 
+
+        body: [
+          HomePage(),
+          ShopPage(),
+          Cart(),
+          FavourtePage(),
+          Profile(),
+
+
+        ][currentItem],
+
+
+
+
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: currentItem,
+          onDestinationSelected: (value) {
+            currentItem = value;
+            setState(() {});
+          },
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(
+                icon: Icon(Icons.shopping_cart), label: 'Shop'),
+            NavigationDestination(icon: Icon(Icons.shopping_bag), label: 'Bag'),
+            NavigationDestination(
+                icon: Icon(Icons.favorite_outline), label: 'Favourites'),
+            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/pages/favourite_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class FavourtePage extends StatelessWidget {
+  const FavourtePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
